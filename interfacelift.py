381e27aa3d003c254c1e171e4d5bdaed4073603a
Denis MacPherson <denismacpherson@gmail.com>
2011-03-31 09:49:10 -0700

    Edited interfacelift.py via GitHub

--- interfacelift.py
+++ interfacelift.py
@@ -2,9 +2,9 @@
 import os
 import urllib2
 
-url       = 'http://interfacelift.com/wallpaper_beta/downloads/date/widescreen/1920x1200/'
-directory = '/datapool/public/photos/1920x1200'
-start     = '<a href="/wallpaper_beta/grab'
+url       = 'http://interfacelift.com/wallpaper/downloads/date/hdtv/1080p/'
+directory = '/datapool/public/wallpaper/1080p'
+start     = '<a href="/wallpaper/i8r4q'  #check interfacelift.com to see what the randomly generated part is at the end
 end       = '">'
 useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
 
@@ -20,7 +20,7 @@ while count < 999999:
                 endofindex = data.find(end, index)
                 currentpos = index + 1
                 urlcount  += 1
-                if os.path.exists(directory + '/0' + data[index+30:endofindex]):
+                if os.path.exists(directory + data[index+25:endofindex]):
                         print 'Directory up to date. Found existing file.'
                         quit()
                 os.system('wget -P ' + directory + ' -nc -U "' + useragent + '" ' + 'http://interfacelift.com' + data[index+9:endofindex])
