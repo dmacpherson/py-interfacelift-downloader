3982a210b78cd5dc2cd4313df3597f7d4fc16315
Øystein Walle <oystwa@gmail.com>
2011-05-26 13:13:07 +0200

    Cleaned up a bit. Added some info about the regex in the README

--- interfacelift.py
+++ interfacelift.py
@@ -5,10 +5,8 @@ import re
 
 url       = 'http://interfacelift.com/wallpaper/downloads/date/hdtv/1080p/'
 directory = '/datapool/public/wallpaper/1080p'
-start     = '<a href="/wallpaper/i8r4q'  #check interfacelift.com to see what the randomly generated part is at the end
-end       = '">'
 useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
-pattern   = '(?<=<a href=")/wallpaper/.*jpg(?=">)'
+pattern   = '(?<=<a href=")/wallpaper/.*jpg(?=">)' # The regex pattern used. Check the README
 
 count     = 1
 while count < 999999:
