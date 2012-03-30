23b2f0093290f8c85a5e17c511d2eede9792ae41
Denis MacPherson <denismacpherson@gmail.com>
2012-03-29 17:21:21 -0700

    Fixed script to stop once it finds an existing file and a option to enable/disable it.

--- interfacelift.py
+++ interfacelift.py
@@ -3,21 +3,32 @@ import os
 import urllib2
 import re
 
-url       = 'http://interfacelift.com/wallpaper/downloads/date/hdtv/1080p/'
-directory = '/datapool/public/wallpaper/1080p'
-useragent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
-pattern   = '(?<=<a href=")/wallpaper/.*jpg(?=">)' # The regex pattern used. Check the README
+# -- Changable Variables
 
+url             = 'http://interfacelift.com/wallpaper/downloads/date/hdtv/1080p/' #Browse to the page that has all the wallpaper you want and paste here
+directory       = '/home/mythuser/1080p' #Path to download to
+useragent       = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)' #Fake useragent since wget is blocked
+pattern         = '(?<=<a href=")/wallpaper/.*jpg(?=">)' # The regex pattern used to look up picture url paths
+picturepattern  = '[^/]*$' # The regex pattern to pull picture filename to see if file exists
+stoponfind      = '1' # Set to 0 to download all files even if the file exists and 1 to stop when it finds where it left off
+
+
+# -- Should not need to edit below here unless something stops working --
 count     = 1
-while count < 999999:
+while count < 9999999:
         data       = urllib2.urlopen(url + "index" + str(count) + ".html").read()
         pictures   = re.findall(pattern, data)
         urlcount   = len(pictures)
         for picture in pictures:
-                if os.path.exists(directory + picture):
-                        print 'Directory up to date. Found existing file.'
-                        quit()
-                os.system('wget -P ' + directory + ' -nc -U "' + useragent + '" ' + 'http://interfacelift.com' + picture)
+                m = re.search(picturepattern, picture)
+                picturefile=m.group()
+                if os.path.exists(directory + "/" + picturefile):
+                        if stoponfind == "1":
+                                print 'Directory up to date.'
+                                quit()
+
+        os.system('wget -P ' + directory + ' -nc -U "' + useragent + '" ' + 'http://interfacelift.com' + picture)
+
         if urlcount == 0:
                 quit()
         count += 1
